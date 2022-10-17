e3df127aef6e350845a207094dea06a0b7a324cf
lroucoux <lroucoux@ippon.fr>
2022-10-17 10:56:07 +0100

    Refacto 1

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -1,36 +1,55 @@
 
 public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    private static final String[] POSSIBLE_SCORE = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+    private static final String DELIMITER = "-";
+    private static final String ALL = "All";
+    private static final String DEUCE = "Deuce";
+    private static final String ADVANTAGE = "Advantage ";
+    private static final String WIN_FOR = "Win for ";
+    private int player1Point;
+    private int player2Point;
+    private final String player1Name;
+    private final String player2Name;
+
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        if (isSimpleScore()) {
+            String score = POSSIBLE_SCORE[player1Point];
+            return isEquality() ? score + DELIMITER + ALL : score + DELIMITER + POSSIBLE_SCORE[player2Point];
         }
+
+        if (isEquality())
+            return DEUCE;
+
+        String playerWithAdvantageOrWin = player1Point > player2Point ? player1Name : player2Name;
+        return isAdvantage() ? ADVANTAGE + playerWithAdvantageOrWin
+                : WIN_FOR + playerWithAdvantageOrWin;
     }
-    
+
+    private boolean isAdvantage() {
+        int ecartPoint = player1Point - player2Point;
+        return ecartPoint * ecartPoint == 1;
+    }
+
+    private boolean isEquality() {
+        return player1Point == player2Point;
+    }
+
+    private boolean isSimpleScore() {
+        return player1Point < 4 && player2Point < 4 && (player1Point + player2Point != 6);
+    }
+
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
+        if (player1Name.equals(playerName))
+            this.player1Point += 1;
         else
-            this.p2 += 1;
-        
+            this.player2Point += 1;
+
     }
 
 }
